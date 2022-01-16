73a137928a13d04f1cac1b8f165792b490a434a6
Anderson Begossi <andersonbegossi@gmail.com>
2022-01-16 15:41:34 -0300

    docs: create Button stories

---
+++ src/components/Button/Button.stories.ts
@@ -0,0 +1,28 @@
+import { Story } from '@storybook/react';
+
+import { Button, ButtonProps } from '.';
+
+type ButtonStory = Story<ButtonProps>;
+
+export default { component: Button, title: 'Button' };
+
+export const Primary: ButtonStory = {
+  args: {
+    children: 'Primary Button',
+  },
+};
+
+export const Secondary: ButtonStory = {
+  args: {
+    children: 'Secondary Button',
+    variant: 'secondary'
+  },
+};
+
+export const Pill: ButtonStory = {
+  args: {
+    children: 'Pill Button',
+    variant: 'secondary',
+    pill: true
+  },
+};
