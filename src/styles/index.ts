0633a96fe8ceca2b1ce547cb5c9eeff89304407d
Anderson Begossi <andersonbegossi@gmail.com>
2022-01-16 11:26:25 -0300

    chore: create stitches theme

---
+++ src/styles/index.ts
@@ -0,0 +1,48 @@
+import { createStitches } from '@stitches/react';
+
+const convertToRem = (value: number) => `${value / 16}rem`;
+
+const space = {
+  4: convertToRem(4),
+  8: convertToRem(8),
+  12: convertToRem(12),
+  16: convertToRem(16),
+  20: convertToRem(20),
+  24: convertToRem(24),
+  28: convertToRem(28),
+  32: convertToRem(32),
+  36: convertToRem(36),
+  40: convertToRem(40),
+}
+
+export const { styled: zusStyled } = createStitches({
+  prefix: 'zus',
+  theme: {
+    colors: {
+      black: '#000000',
+      white: '#ffffff',
+
+      gray800: '#1a1a1a',
+      gray700: '#202020',
+      gray600: '#333333',
+
+      zusRed: '#ff014d',
+
+      background: '#1a1a1a',
+      shape: '#202020',
+
+      'text-default': '#ffffff',
+      'text-highlight': '#ff014d',
+
+      'button-primary-bg': '#ff014d',
+      'button-primary-text': '#ffffff',
+      'button-secondary-bg': 'transparent',
+      'button-secondary-text': '#ffffff',
+    },
+    radius: {
+      default: '4px',
+    },
+    space,
+    sizes: space,
+  },
+});
